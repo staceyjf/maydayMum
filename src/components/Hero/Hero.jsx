892dccf151218686180a85131bb5ab5ea52a8aa1
staceyjf <staceyfanner@gmail.com>
2023-09-15 16:15:30 +1000

    tweak

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,7 @@ import styles from './Hero.module.css'; // bring in specific styling to hero
 function Hero() {
 
     return (
-    <Box className={styles.heroBox} 
-      sx={{ flexGrow: 1, 
-      display: { xs: "none", md: "flex" }}} >
+    <Box className={styles.heroBox} >
           <Grid container sx={{
             display: 'grid',
             gridTemplateColumns: 'repeat(12, 1fr)',
@@ -18,6 +16,7 @@ function Hero() {
             padding: '30px',
             gridRow: '3 / span 4',
             gridColumn: '9 / span 4',
+            flexGrow: 1, display: { xs: "none", md: "flex" }
           }}>
           <Typography variant="h6" sx={{
             textTransform: 'uppercase',
