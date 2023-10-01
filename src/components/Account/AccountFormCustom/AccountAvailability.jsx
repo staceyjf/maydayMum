a266c89b913bd6abf246d893b3669d6a67423c46
staceyjf <staceyfanner@gmail.com>
2023-10-01 13:06:42 +1100

    Date header add

--- src/components/Account/AccountFormCustom/AccountAvailability.jsx
+++ src/components/Account/AccountFormCustom/AccountAvailability.jsx
@@ -8,6 +8,34 @@ function AccountAvailability({user, setUser}) {
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
+
+  // Date header
+  const options = { weekday: 'short', day: 'numeric', month: 'short' };
+
+  function getWeek() {
+    const now = new Date();  // create  a new day object
+    const currentDay = now.getDay(); // gets the current day eg Sunday reps 0
+
+    const daysUntilMonday = currentDay === 0 ? 6 : currentDay - 1; // Calculate the start of the week
+    const startOfWeek = new Date(now);
+    startOfWeek.setDate(now.getDate() - daysUntilMonday);
+    startOfWeek.setHours(0, 0, 0, 0); // Monday starts from 
+
+    const daysUntilSunday = currentDay === 0 ? 0 : 7 - currentDay; // Calculate the end of the  week
+    const endOfWeek = new Date(now);
+    endOfWeek.setDate(now.getDate() + daysUntilSunday);
+    endOfWeek.setHours(23, 59, 59, 999); // Sunday ends at
+
+    return { startOfWeek, endOfWeek };
+  }
+
+  const { startOfWeek, endOfWeek } = getWeek();
+  const dateTimeFormat = new Intl.DateTimeFormat('en', options);
+  const formattedStartDate = dateTimeFormat.format(startOfWeek);
+  const formattedEndDate = dateTimeFormat.format(endOfWeek);
+  const formattedDateRange = `${formattedStartDate} - ${formattedEndDate}`;
+
+
   function handleCheckedChange(evt) { // handles nanny avals checkboxes
     const updatedUser = {
       ...userData.weeklyAvailability,
@@ -45,7 +73,7 @@ function AccountAvailability({user, setUser}) {
         onSubmit={handleSubmit}
       >
         <Card  >
-          <CardHeader title="Weekly Availability" />
+          <CardHeader title={`Availability for ${formattedDateRange}`} />
           <CardHeader subheader="Select your weekly availability" style={{ textAlign: 'left' }} />
           <CardContent sx={{ pt: 0 }}>
             <Box sx={{ m: -1.5 }}>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,35 @@
-import {  Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import styles from './Hero.module.css'; // bring in specific styling to hero
 
 function Hero() {
-
-    return (
-    <Box className={styles.heroBox} >
-          <Grid container sx={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(12, 1fr)',
-            gridTemplateRows: 'repeat(12, 1fr)',
-            height: '100vh',
-          }}>
-          <Grid item sx={{
+  return (
+    <Box className={styles.heroBox}>
+      <Grid
+        container
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(12, 1fr)',
+          gridTemplateRows: 'repeat(12, 1fr)',
+          height: '100vh',
+        }}
+      >
+        <Grid
+          item
+          sx={{
             backgroundColor: 'secondary.main',
             padding: '30px',
+            display: { xs: 'none', md: 'grid' },
             gridRow: '3 / span 4',
-            gridColumn: '9 / span 4'
-          }}>
+            gridColumn: '8 / span 5',
+          }}
+        >
           <Typography variant="h6" sx={{
             textTransform: 'uppercase',
             color: 'white',
             textAlign: 'start',
           }}>
             Trusted childcare
-            </Typography>
+          </Typography>
           <Typography variant="h4" sx={{
             textTransform: 'uppercase',
             textAlign: 'start',
@@ -40,18 +46,56 @@ function Hero() {
             opacity: 0.5,
             paddingTop: '15px',
           }}>
-              Welcome to Mayday Mum – 
-              {/* your trusted Northern Beaches destination for overnight nannying services.  */}
-              we understand that every parent needs a helping hand, 
-              especially during those long nights. Our fixed nightly fee covers a full 12 hours of 
-              dedicated support, so you can rest easy knowing your little ones are in caring hands. 
-              {/* Join our Mayday Mum family today, and let us make your nights more peaceful and 
-              your mornings brighter. */}
-            </Typography>
-          </Grid>
+            Welcome to Mayday Mum. <br />
+            We understand that every parent needs a helping hand,
+            especially during those long nights. <br />
+            Our fixed nightly fee covers a full 12 hours of
+            dedicated support, so you can rest easy knowing your little ones are in caring hands.
+          </Typography>
         </Grid>
-      </Box>
-    );
-  };
+        <Grid
+          item
+          sx={{
+            backgroundColor: 'secondary.main',
+            padding: '30px',
+            display: { xs: 'grid', md: 'none' },
+            gridRow: '5',
+            gridColumn: '2 / span 10',
+          }}
+        >
+          <Typography variant="h6" sx={{
+            textTransform: 'uppercase',
+            color: 'white',
+            textAlign: 'start',
+          }}>
+            Trusted childcare
+          </Typography>
+          <Typography variant="h4" sx={{
+            textTransform: 'uppercase',
+            textAlign: 'start',
+            fontSize: '3vmax',
+            color: 'black',
+            fontWeight: 'bold',
+            paddingTop: '5px',
+          }}>
+            Connect with top overnight nannies in the Northern Beaches.
+          </Typography>
+          <Typography variant="body1" sx={{
+            textAlign: 'justify',
+            color: 'white',
+            opacity: 0.5,
+            paddingTop: '15px',
+          }}>
+            Welcome to Mayday Mum. <br />
+            We understand that every parent needs a helping hand,
+            especially during those long nights. <br />
+            Our fixed nightly fee covers a full 12 hours of
+            dedicated support, so you can rest easy knowing your little ones are in caring hands.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
 
-export default Hero
+export default Hero;
